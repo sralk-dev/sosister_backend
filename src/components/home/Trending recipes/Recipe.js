178343b4e9f988afb54942c19e357ff5af2a4e2f
DenisIsmagilov1 <denchikism789@gmail.com>
2020-02-23 15:12:58 +0300

    Rework TrendingRecipesBox, add Recipe component

---
+++ src/components/home/Trending recipes/Recipe.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import RatingStars from '../../Widgets/Recipes/RatingStars'
+import MetaRecipe from '../../Widgets/Recipes/MetaRecipe'
+
+export class Recipe extends Component {
+  render() {
+
+    const {info} = this.props;
+
+    return (
+      <div class={info.isTop ? "col-12" : "col-md-6 col-sm-6 col-12"}>
+        <div class="product-box-layout1">
+          <figure class="item-figure">
+            <a href="single-recipe1.html">
+              <img src={info.img} alt="Product"/>
+            </a>
+          </figure>
+          <div class="item-content">
+            <span class="sub-title">{info.category}</span>
+            <h2 class="item-title">
+              <a href="single-recipe1.html">{info.title}</a>
+            </h2>
+            <RatingStars score={info.score}/>
+            <p>{info.description}</p>
+            <MetaRecipe owner={info.owner} mins={info.mins} likes={info.likes}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Recipe
--- src/components/home/Trending recipes/TrendingRecipesBox.js
+++ src/components/home/Trending recipes/TrendingRecipesBox.js
@@ -1,8 +1,46 @@
-import React, { Component } from 'react'
-import TopRecipe from './TopRecipe'
+import React, { Component } from 'react';
+import Recipe from './Recipe';
+
+let dummyData = [
+  {
+    "title": "Sosiski na paru",
+    "category": "La pasta",
+    "img": "img/product/product4.jpg",
+    "description": "More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously side crud mightily rigorous plot life. New homes n particular are subject.",
+    "score": 3,
+    "owner": 'Petr',
+    "likes": 23,
+    "mins": 2
+  },
+  {
+    "title": "Value everybody",
+    "category": "La la",
+    "img": "img/product/product4.jpg",
+    "description": "More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously side crud mightily rigorous plot life. New homes n particular are subject.",
+    "score": 5,
+    "owner": 'Anna',
+    "likes": 3,
+    "mins": 233
+  },
+  {
+    "title": "Purewka",
+    "category": "La pure",
+    "img": "img/product/product4.jpg",
+    "description": "More off this less hello salamander lied porpoise much over tightly circa horse taped so innocuously side crud mightily rigorous plot life. New homes n particular are subject.",
+    "score": 1,
+    "owner": 'Pawa',
+    "likes": 1000,
+    "mins": 15
+  }
+];
 
 export class TrendingRecipesBox extends Component {
   render() {
+
+    dummyData[0]['isTop'] = true;
+
+    const listRecipes = dummyData.map(recipe => <Recipe info={{...recipe}}/>);
+
     return (
       <section class="padding-bottom-45">
         <div class="container">
@@ -12,7 +50,7 @@ export class TrendingRecipesBox extends Component {
                 <h2 class="item-heading">TRENDING RECIPES</h2>
               </div>
               <div class="row">
-                <TopRecipe/>
+                {listRecipes}
               </div>
             </div>
           </div>
