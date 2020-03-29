ef2e7e79bc3c5f78ec4c386cded0884d5d77730a
DenisIsmagilov1 <denchikism789@gmail.com>
2020-03-29 10:47:58 +0300

    Add skelet app

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      Hello Sosister!
+      <Switch>
+        <Route exact path='/'>
+          Home
+        </Route>
+      </Switch>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/app';
+import ErrorBoundry from './components/error-boundry';
+
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <Router>
+        <App />
+      </Router>
+    </ErrorBoundry>
+  </Provider>
+  , document.getElementById('root'));
