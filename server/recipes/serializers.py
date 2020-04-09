e1211e4455c0f7b4551bfad798925fbcff348ecc
Pavel Khokhlov <hpawa6@gmail.com>
2020-04-10 01:09:13 +0700

    Сделал сериализатору рецептов динамические поля

--- server/recipes/serializers.py
+++ server/recipes/serializers.py
@@ -3,28 +3,51 @@ from .models import Recipe, Grade, Category
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop('fields', None)
+        exclude = kwargs.pop('exclude', None)
+
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+        if exclude:
+            disallowed = set(exclude)
+            existing = set(self.fields)
+            for field_name in disallowed:
+                if field_name in existing:
+                    self.fields.pop(field_name)
+        
+
+
+class UserSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username')
+        fields = '__all__'
 
 
-class GradeSerializer(serializers.ModelSerializer):
+class GradeSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = Grade
-        fields = ('id', 'value')
+        fields = '__all__'
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = Category
-        exclude = ('description', 'poster')
+        fields = '__all__'
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    owner = UserSerializer(read_only=True)
-    grades = GradeSerializer(read_only=True, many=True)
-    categories = CategorySerializer(read_only=True, many=True)
+    #TODO: single&list mode
+    owner = UserSerializer(read_only=True, fields=('id', 'username'))
+    grades = GradeSerializer(read_only=True, many=True, fields=('id', 'value'))
+    categories = CategorySerializer(read_only=True, many=True, exclude=('description', 'poster'))
 
     class Meta:
         model = Recipe
